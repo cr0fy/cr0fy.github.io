a44e75e433be3c62ee547d18604710c07d578210
cr0fy <787078591@qq.com>
2021-09-10 00:02:28 +0800

    Site updated: 2021-09-10 00:02:27

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,17 +1,18 @@
-<!--崩溃欺骗-->
+<!--浏览器搞笑标题-->
  var OriginTitle = document.title;
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/images/TEP.ico");
+         $('[rel="icon"]').attr('href', "images/TEP.ico");
          document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/images/TEP.ico");
+         $('[rel="icon"]').attr('href', "images/TEP.ico");
          document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
      }
  });
+
